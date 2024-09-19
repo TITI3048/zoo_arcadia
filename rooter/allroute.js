fc6608243b06c53b0d5361508b2f056c9ffedc00
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-20 00:52:53 +0200

    modif

--- rooter/allroute.js
+++ rooter/allroute.js
@@ -3,7 +3,7 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/", "Services", "/pages/services.html"),
+    new Route("/services", "Services", "/pages/services.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
