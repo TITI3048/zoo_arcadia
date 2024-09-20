da30ba5c8cea585033bab7a4398cc8ff7d1a1011
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-20 02:10:29 +0200

    creation page jungle

--- rooter/allroute.js
+++ rooter/allroute.js
@@ -3,6 +3,7 @@ import Route from "./route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/services", "Services", "/pages/services.html"),
+    new Route("/jungle", "Jungle", "/pages/jungle.html"),
 ];
 
 export const websiteName = "Zoo Arcadia";
