40ed958495342eb4a2bc228a95d5f7948e399b9a
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-20 12:00:48 +0200

    modification et creation

--- rooter/allroute.js
+++ rooter/allroute.js
@@ -4,8 +4,8 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/services", "Services", "/pages/services.html"),
     new Route("/jungle", "Jungle", "/pages/jungle.html", "/js/jungle.js"),
-    new Route("/savane", "Savane", "/pages/savane.html"),
-    new Route("/marais", "Marais", "/pages/marais.html"),
+    new Route("/savane", "Savane", "/pages/savane.html", "/js/savane.js"),
+    new Route("/marais", "Marais", "/pages/marais.html", "/js/marais.js"),
     new Route("/connexion", "Connexion", "/pages/connexion.html"),
 ];
 
