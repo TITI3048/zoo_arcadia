abadcbb490f8710d14dc95fb1749023217e97725
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-20 08:12:05 +0200

    création page marais

--- rooter/allroute.js
+++ rooter/allroute.js
@@ -5,6 +5,7 @@ export const allRoutes = [
     new Route("/services", "Services", "/pages/services.html"),
     new Route("/jungle", "Jungle", "/pages/jungle.html"),
     new Route("/savane", "Savane", "/pages/savane.html"),
+    new Route("/marais", "Marais", "/pages/marais.html"),
 ];
 
 export const websiteName = "Zoo Arcadia";
