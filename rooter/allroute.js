d0157e894ed924816b4c846d09f35c530faee1be
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-20 02:21:34 +0200

    creation page savane

--- rooter/allroute.js
+++ rooter/allroute.js
@@ -4,6 +4,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/services", "Services", "/pages/services.html"),
     new Route("/jungle", "Jungle", "/pages/jungle.html"),
+    new Route("/savane", "Savane", "/pages/savane.html"),
 ];
 
 export const websiteName = "Zoo Arcadia";
